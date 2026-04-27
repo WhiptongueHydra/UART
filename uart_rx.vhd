348e4382b2c7c28e9cde1c423aec7e3d5e33abcb
WhiptongueHydra <stephencardwell@protonmail.com>
2026-04-27 20:31:59 +0100

    Updated uart rx

--- uart_rx.vhd
+++ uart_rx.vhd
@@ -7,29 +7,43 @@ entity rx is
        		clk: in std_logic;
 		rst: in std_logic;
 		
+       		baud_req: out std_logic;
        		baud_in: in std_logic;
        		rx: in std_logic;
-		
+
 		byte_received: out std_logic_vector(7 downto 0);
        		err: in std_logic -- For parity errors
 	);
 end entity rx;
 
 architecture A1 of rx is
-	type uart_state is (idle, receiving, done);
+	type uart_state is (idle, receiving);
 	signal p_state, n_state: uart_state := idle;
 	-- Enough room for data and parity
 	signal shift_reg: std_logic_vector(8 downto 0) := (others => '1');
+	signal latched_rx: std_logic_vector(8 downto 0) := (others => '1');
+
+	signal count_en: std_logic := '0'; 
+	constant max_count: integer := 8; 
+	signal counter: integer range 0 to 8 := 0;
+
+	signal parity_int: std_logic := '0';
 begin
+	byte_received <= latched_rx(8 downto 1);
+
 	shifter_proc: process(clk)
 	begin
 		if rising_edge(clk) then
 			if rst='1' then
 				shift_reg <= (others => '1');
 			else
+				if done='1' then
+					latched_rx <= shift_reg;
+				end if;		
+				
 				if baud_in='1' then
 					shift_reg <= shift_reg(8 downto 1) & rx;
-				end if;	
+				end if;						
 			end if;
 		end if;
 	end process shifter_proc;
@@ -41,23 +55,48 @@ begin
 		end if;
 	end process sync_state_changer;
 
-	comb_proc: process(p_state, rx) 
+	counter_proc: process(clk)
 	begin
+		if rising_edge(clk) then
+			if rst='1' then
+				counter <= 0;
+			else
+				done <= '0';
+				if baud_en='1' then
+					if baud='1' then
+						if counter < max_count then
+							counter <= counter + 1;
+						else
+							counter <= 0;
+							done <= '1';
+						end if;
+					end if;
+				end if;
+			end if;
+		end if;
+	end process counter_proc;
+
+	-- Need to add baud rate requester tomorrow to external module
+	-- Also need to write test bench
+	comb_proc: process(p_state, rx, done) 
+	begin
+		n_state <= p_state;
+		count_en <= '0';
+		baud_en <= '0';
 		case p_state is
 			when idle =>
+				baud_req <= '0'
 				if rx='0' then
-					n_state <= receiving;
-				else
-					n_state <= idle;	
+					n_state <= receiving;	
 				end if;	
 			when receiving =>
-				
-			when done =>
-
+				baud_en <= '1'
+				if done='1' then
+					n_state <= idle;
+				end if;		
 			when others =>
 				NULL;
 		end case;
 	end process comb_proc;
-
-
 end architecture A1;
+
